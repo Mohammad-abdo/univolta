f00f00fc1f1d2fd40354dd26174540cac2f7c9c8
Mohammad Abdlrahman Sad <91919000+Mohammad-abdo@users.noreply.github.com>
2025-12-18 19:21:43 +0200

    Update constants.ts
    dsd

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000/api/v1";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://univolta.developteam.site/api/v1";
 
 
